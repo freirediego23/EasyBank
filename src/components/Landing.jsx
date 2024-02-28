08ba73ae982bdf2d828c6382c884fa37fdeafc89
freirediego <diegofg23@outlook.es>
2024-02-27 22:55:37 -0500

    updated site

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -52,80 +52,82 @@ function Landing() {
       </div>
 
       <div className="grayback pb-20">
-        <div className="top-header">
-          <h1 className="text-center text-5xl ml-2 mr-2 ">
-            Why choose Easybank?
-          </h1>
-          <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold">
-            We leverage Open Banking to turn your bank account into your
-            financial hub. Control your finances like never before.
-          </p>
-        </div>
-
-        {/* Paragraphs below easybank */}
-        <div className="grid-container">
-          <div className="para-cont">
-            <div className="flex justify-center lg:justify-start mt-8">
-              <img src={Online} alt="card image" className="text-center" />
-            </div>
-
-            <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
-              Online Banking
+        <div className="gray-wrap">
+          <div className="top-header">
+            <h1 className="text-center text-5xl ml-2 mr-2 lg:ml-0 lg:text-left">
+              Why choose Easybank?
             </h1>
-            <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
-              Our modern web and mobile applications allow you to keep track of
-              your finances wherever you are in the world.
+            <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:text-left lg:ml-0">
+              We leverage Open Banking to turn your bank account into your
+              financial hub. Control your finances like never before.
             </p>
           </div>
 
-          <div className="para-cont">
-            <div className="flex justify-center lg:justify-start mt-8">
-              <img
-                src={Budget}
-                alt="card image"
-                className="text-center lg:text-left"
-              />
+          {/* Paragraphs below easybank */}
+          <div className="grid-container">
+            <div className="para-cont">
+              <div className="flex justify-center lg:justify-start mt-8">
+                <img src={Online} alt="card image" className="text-center" />
+              </div>
+
+              <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
+                Online Banking
+              </h1>
+              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
+                Our modern web and mobile applications allow you to keep track
+                of your finances wherever you are in the world.
+              </p>
             </div>
 
-            <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
-              Simple Budgeting
-            </h1>
-            <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
-              See exactly where your money goes each month. Receive
-              notifications when you’re close to hitting your limits.
-            </p>
-          </div>
+            <div className="para-cont">
+              <div className="flex justify-center lg:justify-start mt-8">
+                <img
+                  src={Budget}
+                  alt="card image"
+                  className="text-center lg:text-left"
+                />
+              </div>
 
-          <div className="para-cont">
-            <div className="flex justify-center lg:justify-start mt-8">
-              <img
-                src={Onboarding}
-                alt="card image"
-                className="text-center lg:text-left"
-              />
+              <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
+                Simple Budgeting
+              </h1>
+              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
+                See exactly where your money goes each month. Receive
+                notifications when you’re close to hitting your limits.
+              </p>
             </div>
 
-            <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
-              Fast Onboarding
-            </h1>
-            <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
-              We don’t do branches. Open your account in minutes online and
-              start taking control of your finances right away.
-            </p>
-          </div>
+            <div className="para-cont">
+              <div className="flex justify-center lg:justify-start mt-8">
+                <img
+                  src={Onboarding}
+                  alt="card image"
+                  className="text-center lg:text-left"
+                />
+              </div>
 
-          <div className="para-cont">
-            <div className="flex justify-center lg:justify-start mt-8">
-              <img src={Icon_api} alt="card image" className="text-center" />
+              <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
+                Fast Onboarding
+              </h1>
+              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
+                We don’t do branches. Open your account in minutes online and
+                start taking control of your finances right away.
+              </p>
             </div>
 
-            <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
-              Open API
-            </h1>
-            <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
-              Manage your savings, investments, pension, and much more from one
-              account. Tracking your money has never been easier.
-            </p>
+            <div className="para-cont w-80">
+              <div className="flex justify-center lg:justify-start mt-8">
+                <img src={Icon_api} alt="card image" className="text-center" />
+              </div>
+
+              <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0 lg:mr-0">
+                Open API
+              </h1>
+              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0 lg:mr-0">
+                Manage your savings, investments, pension, and much more from
+                one account. Tracking your money has never been easier.
+              </p>
+            </div>
           </div>
         </div>
       </div>
