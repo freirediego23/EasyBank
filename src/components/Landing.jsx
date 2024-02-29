5db1349c8341dd2de26d2784d38e52a598cac147
freirediego <diegofg23@outlook.es>
2024-02-29 00:27:44 -0500

    updated site

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -53,7 +53,7 @@ function Landing() {
 
       <div className="grayback pb-20">
         <div className="gray-wrap">
-          <div className="top-header">
+          <div className="top-header lg:mb-5">
             <h1 className="text-center text-5xl ml-2 mr-2 lg:ml-0 lg:text-left">
               Why choose Easybank?
             </h1>
@@ -73,7 +73,7 @@ function Landing() {
               <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
                 Online Banking
               </h1>
-              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
+              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0 lg:mr-0">
                 Our modern web and mobile applications allow you to keep track
                 of your finances wherever you are in the world.
               </p>
@@ -91,7 +91,7 @@ function Landing() {
               <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
                 Simple Budgeting
               </h1>
-              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
+              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0 lg:mr-0">
                 See exactly where your money goes each month. Receive
                 notifications when you’re close to hitting your limits.
               </p>
@@ -109,13 +109,13 @@ function Landing() {
               <h1 className="text-center text-2xl ml-2 mr-2 mt-8 lg:ml-0">
                 Fast Onboarding
               </h1>
-              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0">
+              <p className="text-center ml-12 mr-12 text-lg mt-4 low-gray font-bold lg:ml-0 lg:mr-0">
                 We don’t do branches. Open your account in minutes online and
                 start taking control of your finances right away.
               </p>
             </div>
 
-            <div className="para-cont w-80">
+            <div className="para-cont ">
               <div className="flex justify-center lg:justify-start mt-8">
                 <img src={Icon_api} alt="card image" className="text-center" />
               </div>
@@ -134,66 +134,97 @@ function Landing() {
 
       {/* Latest Articles Section */}
 
-      <h1 className="text-center text-5xl ml-2 mr-2 mt-20">Latest Articles</h1>
+      <div className="article-container gray-wrap">
+        <h1 className="text-center text-5xl ml-2 mr-2 mt-20 lg:ml-0 lg:text-left lg:mb-8">
+          Latest Articles
+        </h1>
 
-      <div className="flex justify-center mt-8 img-cont ml-6 mr-6">
-        <img src={Dollars} alt="card image" className="text-center resize" />
-      </div>
+        <div className="grid-container">
+          <div className="grid-item">
+            <div className="flex justify-center mt-8 img-cont ml-6 mr-6 lg:ml-0 lg:mr-0 lg:h-56">
+              <img
+                src={Dollars}
+                alt="card image"
+                className="text-center resize"
+              />
+            </div>
 
-      <div className="article ml-20 mr-20 mt-6 mb-12">
-        <p className="text-sm">By Claire Robinson</p>
-        <h3 className="text-xl font-bold mt-2 mb-2">
-          Receive money in any currency with no fees
-        </h3>
-        <p>
-          The world is getting smaller and we’re becoming more mobile. So why
-          should you be forced to only receive money in a single …
-        </p>
-      </div>
+            <div className="article ml-20 mr-20 mt-6 mb-12 lg:ml-5 lg:mr-0">
+              <p className="text-sm">By Claire Robinson</p>
+              <h3 className="text-xl font-bold mt-2 mb-2">
+                Receive money in any currency with no fees
+              </h3>
+              <p>
+                The world is getting smaller and we’re becoming more mobile. So
+                why should you be forced to only receive money in a single …
+              </p>
+            </div>
+          </div>
 
-      <div className="flex justify-center mt-8 img-cont ml-6 mr-6">
-        <img src={Restaurant} alt="card image" className="text-center resize" />
-      </div>
+          <div className="grid-item">
+            <div className="flex justify-center mt-8 img-cont ml-6 mr-6 lg:ml-0 lg:mr-0 lg:h-56">
+              <img
+                src={Restaurant}
+                alt="card image"
+                className="text-center resize"
+              />
+            </div>
 
-      <div className="article ml-20 mr-20 mt-6 mb-12">
-        <p className="text-sm">By Wilson Hutton</p>
-        <h3 className="text-xl font-bold mt-2 mb-2">
-          Treat yourself without worrying about money
-        </h3>
-        <p>
-          Our simple budgeting feature allows you to separate out your spending
-          and set realistic limits each month. That means you …
-        </p>
-      </div>
+            <div className="article ml-20 mr-20 mt-6 mb-12 lg:ml-5 lg:mr-10">
+              <p className="text-sm">By Wilson Hutton</p>
+              <h3 className="text-xl font-bold mt-2 mb-2">
+                Treat yourself without worrying about money
+              </h3>
+              <p>
+                Our simple budgeting feature allows you to separate out your
+                spending and set realistic limits each month. That means you …
+              </p>
+            </div>
+          </div>
 
-      <div className="flex justify-center mt-8 img-cont ml-6 mr-6">
-        <img src={Plane} alt="card image" className="text-center resize" />
-      </div>
+          <div className="grid-item">
+            <div className="flex justify-center mt-8 img-cont ml-6 mr-6 lg:ml-0 lg:mr-0 lg:h-56">
+              <img
+                src={Plane}
+                alt="card image"
+                className="text-center resize"
+              />
+            </div>
 
-      <div className="article ml-20 mr-20 mt-6 mb-12">
-        <p className="text-sm">By Wilson Hutton</p>
-        <h3 className="text-xl font-bold mt-2 mb-2">
-          Take your Easybank card wherever you go
-        </h3>
-        <p>
-          We want you to enjoy your travels. This is why we don’t charge any
-          fees on purchases while you’re abroad. We’ll even show you …
-        </p>
-      </div>
+            <div className="article ml-20 mr-20 mt-6 mb-12 lg:ml-5 lg:mr-10">
+              <p className="text-sm">By Wilson Hutton</p>
+              <h3 className="text-xl font-bold mt-2 mb-2">
+                Take your Easybank card wherever you go
+              </h3>
+              <p>
+                We want you to enjoy your travels. This is why we don’t charge
+                any fees on purchases while you’re abroad. We’ll even show you …
+              </p>
+            </div>
+          </div>
 
-      <div className="flex justify-center mt-8 img-cont ml-6 mr-6">
-        <img src={Confetti} alt="card image" className="text-center resize" />
-      </div>
+          <div className="grid-item">
+            <div className="flex justify-center mt-8 img-cont ml-6 mr-6 lg:ml-0 lg:mr-0 lg:h-56">
+              <img
+                src={Confetti}
+                alt="card image"
+                className="text-center resize"
+              />
+            </div>
 
-      <div className="article ml-20 mr-20 mt-6 mb-40">
-        <p className="text-sm">By Claire Robinson</p>
-        <h3 className="text-xl font-bold mt-2 mb-2">
-          Our invite-only Beta accounts are now live!
-        </h3>
-        <p>
-          After a lot of hard work by the whole team, we’re excited to launch
-          our closed beta. It’s easy to request an invite through the site ...
-        </p>
+            <div className="article ml-20 mr-20 mt-6 mb-40 lg:ml-5 lg:mr-7">
+              <p className="text-sm">By Claire Robinson</p>
+              <h3 className="text-xl font-bold mt-2 mb-2">
+                Our invite-only Beta accounts are now live!
+              </h3>
+              <p>
+                After a lot of hard work by the whole team, we’re excited to
+                launch our closed beta. It’s easy to request an invite through
+                the site ...
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <Footer></Footer>
